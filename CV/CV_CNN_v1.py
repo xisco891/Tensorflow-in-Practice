b946fa19e1681ace05bb61e766905942436f4a35
Francisco Belenguer <pabelcer@hotmail.com>
2020-01-06 12:47:33 +0100

    Tensorflow in

---
+++ CV/CV_CNN_v1.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 26 11:49:39 2019
+
+@author: Francisco
+"""
+
+####Old Code Version
+
+import tensorflow as tf
+print(tf.__version__)
+
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('loss')<0.4):
+      print("\nReached 60% accuracy so cancelling training!")
+      self.model.stop_training = True
+
+callbacks = myCallback()
+mnist = tf.keras.datasets.fashion_mnist
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+training_images=training_images/255.0
+test_images=test_images/255.0
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
+model.fit(training_images, training_labels, epochs=5, callbacks=[callbacks])
+
+
+#### New Version with CNN
+
+import tensorflow as tf
+print(tf.__version__)
+
+mnist = tf.keras.datasets.fashion_mnist
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+
+
+training_images=training_images.reshape(60000, 28, 28, 1)
+training_images=training_images / 255.0
+
+test_images = test_images.reshape(10000, 28, 28, 1)
+test_images=test_images/255.0
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(28, 28, 1)),
+  tf.keras.layers.MaxPooling2D(2, 2),
+  tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+  tf.keras.layers.MaxPooling2D(2,2),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+
+  model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+model.fit(training_images, training_labels, epochs=5)
+
+test_loss = model.evaluate(test_images, test_labels)
+
+
+print(test_labels[:100])
+
+##### Visualization layers CNN model. 
+
+import matplotlib.pyplot as plt
+f, axarr = plt.subplots(3,4)
+
+FIRST_IMAGE=0
+SECOND_IMAGE=7
+THIRD_IMAGE=26
+CONVOLUTION_NUMBER = 1
+
+from tensorflow.keras import models
+layer_outputs = [layer.output for layer in model.layers]
+activation_model = tf.keras.models.Model(inputs = model.input, outputs = layer_outputs)
+for x in range(0,4):
+  f1 = activation_model.predict(test_images[FIRST_IMAGE].reshape(1, 28, 28, 1))[x]
+  axarr[0,x].imshow(f1[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
+  axarr[0,x].grid(False)
+  f2 = activation_model.predict(test_images[SECOND_IMAGE].reshape(1, 28, 28, 1))[x]
+  axarr[1,x].imshow(f2[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
+  axarr[1,x].grid(False)
+  f3 = activation_model.predict(test_images[THIRD_IMAGE].reshape(1, 28, 28, 1))[x]
+  axarr[2,x].imshow(f3[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
+  axarr[2,x].grid(False)
+
+
+##### Final Exercise : What effects do we have on the metrics when we change the number of filters in the CNN?. 
+  
+import tensorflow as tf
+print(tf.__version__)
+mnist = tf.keras.datasets.mnist
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+training_images=training_images.reshape(60000, 28, 28, 1)
+training_images=training_images / 255.0
+test_images = test_images.reshape(10000, 28, 28, 1)
+test_images=test_images/255.0
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(28, 28, 1)),
+  tf.keras.layers.MaxPooling2D(2, 2),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(training_images, training_labels, epochs=10)
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print(test_acc)
+
+
+
+
+
